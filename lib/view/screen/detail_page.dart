3b0879859a9b3c1cfde2a44c4c185d89ef4817e3
tushal kaklotar <tushalkaklotar90@gmail.com>
2023-07-15 16:52:37 +0530

    KT || 15/7/23

--- lib/view/screen/detail_page.dart
+++ lib/view/screen/detail_page.dart
@@ -1,22 +1,18 @@
+import 'package:bhagavat_geeta_app/controller/json_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import '../../Modals/gita_modals.dart';
-import '../../controller/json_provider.dart';
 
 class Detail_Page extends StatelessWidget {
   const Detail_Page({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<JSONProvider>(builder: (context,p,_) {
-      return p.allGita.isNotEmpty
-          ? AppBar(
-      ) : Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-    );
+    dynamic index = ModalRoute.of(context)!.settings.arguments;
+   return Scaffold(
+     appBar: AppBar(
+       title: Text(Provider.of<JSONProvider>(context).allGita[index].name),
+     ),
+   );
   }
 }
 //GitaModal data = p.allGita[index];
